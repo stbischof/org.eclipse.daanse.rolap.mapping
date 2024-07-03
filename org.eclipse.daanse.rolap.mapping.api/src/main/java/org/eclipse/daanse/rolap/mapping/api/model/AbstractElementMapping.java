39b51d944c3887f6660b8199ba0c35ee4cda092f
Stefan Bischof <stbischof@bipolis.org>
2024-07-03 09:15:17 +0200

    add id to identify and use name for naming, rm caption
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- org.eclipse.daanse.rolap.mapping.api/src/main/java/org/eclipse/daanse/rolap/mapping/api/model/AbstractElementMapping.java
+++ org.eclipse.daanse.rolap.mapping.api/src/main/java/org/eclipse/daanse/rolap/mapping/api/model/AbstractElementMapping.java
@@ -18,7 +18,7 @@ public interface AbstractElementMapping {
 
     List<? extends AnnotationMapping> getAnnotations();
 
-    String getCaption();
+    String getId();
 
     String getDescription();
 
